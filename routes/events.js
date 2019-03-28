70bc51bb27133b586d76a908e0d98c77987b45c3
pmckeegan <pmckeegan@gmail.com>
2019-03-28 20:24:08 +0000

    created route events.js

--- routes/events.js
+++ routes/events.js
@@ -6,12 +6,13 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+    res.render ("create");
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
   });
 
   return router;
