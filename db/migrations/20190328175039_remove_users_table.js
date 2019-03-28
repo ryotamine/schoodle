cc48c56836a3ff976cd67f4861ccd1c22cec3eeb
pmckeegan <pmckeegan@gmail.com>
2019-03-28 17:55:26 +0000

    removed users table in database

---
+++ db/migrations/20190328175039_remove_users_table.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.dropTable('users');
+
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.createTable('users', function (table) {
+        table.increments();
+        table.string('name');
+      });
+};
