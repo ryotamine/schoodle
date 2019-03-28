d90bfe3fba7ad2e24cfd15106b0ccd785be5a110
Ryota Mine <ryotamine@gmail.com>
2019-03-28 22:57:43 +0000

    organized routes sequence

--- routes/events.js
+++ routes/events.js
@@ -18,52 +18,47 @@ module.exports = (knex) => {
 
 }
 
-// GET
-
 // GET create event page
-  router.get("/event", (req, res) => {
-    res.render("event");
-  });
-  
-// GET host_confirmation page
-  router.get("/event/:event_id/confirmation", (req, res) => {
-    res.render("host_confirmation");
-  });
-
-// GET event_URL page
-  router.get("/event/:uniqueURL", (req, res) => {
-    res.render("event_URL")
-  });
-
-  // GET guest_confirmation page REVIEW W MENTORS!!!!!!!!!!!!!!!!!!!
-    router.get("/event/:uniqueURL/guest_confirmation", (req, res) => {
-      res.render("guest_confirmation")
-    });
-
-  // GET event_results page
-  router.get("/event/:uniqueURL/results", (req, res) => {
-    res.render("event_results")
-  });
-
-  router.get("/event/:uniqueURL/guest_confirmation/modify", (req, res) => {
-    res.render("event_modify")
-  });
-
-
-
-
-// POST
+router.get("/event", (req, res) => {
+  res.render("event");
+});
 
 // POST create event page
-  router.post("/event", (req, res) => {
-    res.redirect("/event/:event_id/host_confirmation");
-  });
+router.post("/event", (req, res) => {
+  res.redirect("/event/:event_id/host_confirmation");
+});
 
-// PUT for poll page (event_URL) REVIEW W MENTOR!!!!!!!!
-  router.post("/event/:event_id", (req, res) => {
-    res.redirect("/event/:event_id/guest_confirmation");
-  });
+// GET host_confirmation page
+router.get("/event/:event_id/confirmation", (req, res) => {
+  res.render("host_confirmation");
+});
 
-  router.post("/event/:event_id/modify", (req, res) => {
-    res.redirect("/event/:event_id/guest_confirmation");
-  });
+// GET event_URL page
+router.get("/event/:uniqueURL", (req, res) => {
+  res.render("event_URL");
+});
+
+// POST event_URL page
+router.post("/event/:uniqueURL", (req, res) => {
+  res.redirect("/event/:uniqueURL/guest_confirmation");
+});
+
+// GET guest_confirmation page
+router.get("/event/:uniqueURL/guest_confirmation", (req, res) => {
+  res.render("guest_confirmation");
+});
+
+// GET event_results page
+router.get("/event/:uniqueURL/results", (req, res) => {
+  res.render("event_results");
+});
+
+// GET event_modify page
+router.get("/event/:uniqueURL/guest_confirmation/modify", (req, res) => {
+  res.render("event_modify");
+});
+
+// POST event_modify page
+router.post("/event/:event_id/modify", (req, res) => {
+  res.redirect("/event/:event_id/guest_confirmation");
+});
