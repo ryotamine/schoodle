37e6bf22b7aa868b5a04074c7a574a8f2f5d2893
pmckeegan <pmckeegan@gmail.com>
2019-03-29 17:22:18 +0000

    calendar outputs the date chosen

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
   console.log("document ready calendar.js");
   flatpickr("#flatpickr", {
     enableTime: true,
-    dateFormat: "F, d Y H:i"
+    dateFormat: "F, d Y H:i",
+    onChange: function(selectedDates, dateStr, instance) {
+      console.log(selectedDates, dateStr, instance)
+      //...
+  },
   });
 });
-
