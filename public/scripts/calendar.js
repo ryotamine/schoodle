fd186c38445bb5e2bdd1b9e0339aaf986faf1cdf
pmckeegan <pmckeegan@gmail.com>
2019-03-30 00:45:00 +0000

    calendar prints date choices in order

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,13 +1,27 @@
+
 $(document).ready(function() { 
 
-  var dateOptions=[];
-  flatpickr("#flatpickr", {
+  // pg.insert(knex.select().from('options_date').insert({date_option1: dateChoices.flatpickr1}))
+  // pg.insert(knex.select().from('options_date').insert({date_option2: dateChoices.flatpickr2}))
+  // pg.insert(knex.select().from('options_date').insert({date_option3: dateChoices.flatpickr3}))
+  
+  var dateChoices={};
+
+  flatpickr(".flatpickr", {
     enableTime: true,
     dateFormat: "F, d Y H:i",
-    onClose: function(selectedDates, dateStr, instance) {
-      console.log(selectedDates[0])
-      dateOptions.push(selectedDates[0]);
-    },
+    onClose: function(selectedDates) {
+      var id = ($(this.element).attr('id'));
+      dateChoices[id]=(selectedDates[0]);
+      Object.values(dateChoices);
+      
+      console.log(dateChoices.flatpickr1);
+      console.log(dateChoices.flatpickr2);
+      console.log(dateChoices.flatpickr3);
+
+      $("#createEvent").click(function() {
+
+      });
+    }   
   });
-  console.log(dateOptions);
 });
