e445a3f4f7f34fabeb03ff7f6bd17276fdfc90b1
pmckeegan <pmckeegan@gmail.com>
2019-03-31 00:36:05 +0000

    added votecount column, removed time_options

---
+++ db/migrations/20190331002808_add_votecount_to_options_date.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+return knex.schema.table('options_date', function(table){
+    table.integer('votecount').notNull().defaultTo(0);
+});
+}; 
+
+exports.down = function(knex, Promise) {
+ return knex.schema.table('options_date', function(table){
+     table.dropColumn('votecount');
+ }); 
+};
